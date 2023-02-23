b78f2924c327382828c9a3d5053ba46bfbb0d664
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-02-22 17:18:54 -0800

    added notes

---
+++ app.js
@@ -0,0 +1,4 @@
+// function to generate random number of customers per hour
+// function getRandomCust (min,mx){
+//returnmath.floor (Math.random)() * (max- min +1) + min)
+//console.log (getRandomCust)
